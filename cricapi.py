2b993099a8d4c311f0c91b008f2f1a311c3fe4e9
rbhatia2308 <rbhatia2308@gmail.com>
2025-06-18 14:32:41 +0530

    Update cricapi.py
    Added API Key

--- cricapi.py
+++ cricapi.py
@@ -1,6 +1,6 @@
 import requests
 
-API_KEY = "your_cricapi_key_here"
+API_KEY = "c6962711-0540-49d3-806d-d23a03ddae59"
 
 def get_matches():
     url = f"https://api.cricapi.com/v1/matches?apikey={API_KEY}"
